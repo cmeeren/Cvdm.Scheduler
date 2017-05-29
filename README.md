cacfb8598c08f6f3bcb610fce6f85b8dc590cf5c
Christer van der Meeren <cmeeren@protonmail.com>
2017-05-29 13:29:12 +0200

    Update documentation and README

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Cvdm.Scheduler
-Schedules execution of a C# callback.
+Schedules execution of a C# callback using `System.Threading.Timer`. Compatible with .NET Standard ≥1.2.
+
+The library consists of an interface (`IScheduler`) and an implementation (`Scheduler`). The interface makes the scheduler easy to use with dependency injection. The implementation keeps internal references to all created timers, so you don't have to do that yourself if the scheduler lives at least as long as the timer (e.g. if the scheduler is a singleton component).
+
+The following methods/overloads are provided:
+
+### Schedule recurring execution of code
+
+```c#
+// State-accepting callback, starting at a fixed time
+Timer ScheduleRecurring(TimerCallback callback, object state, DateTimeOffset startTime, TimeSpan interval);
+
+// State-accepting callback, starting after a delay
+Timer ScheduleRecurring(TimerCallback callback, object state, TimeSpan startAfter, TimeSpan interval);
+
+// Simple action, starting at a fixed time
+Timer ScheduleRecurring(Action callback, DateTimeOffset startTime, TimeSpan interval);
+
+// Simple action, starting after a delay
+Timer ScheduleRecurring(Action callback, TimeSpan startAfter, TimeSpan interval);
+```
+
+### Schedule one-time execution of code
+
+```c#
+// State-accepting callback, invoked at a fixed time
+Timer ScheduleOneTime(TimerCallback callback, object state, DateTimeOffset startTime)
+  
+// State-accepting callback, invoked after a delay
+Timer ScheduleOneTime(TimerCallback callback, object state, TimeSpan startAfter)
+
+// Simple action, invoked at a fixed time
+Timer ScheduleOneTime(Action callback, DateTimeOffset startTime)
+
+// Simple action, invoked after a delay
+Timer ScheduleOneTime(Action callback, TimeSpan startAfter)
+```
+
--- Scheduler/src/IScheduler.cs
+++ Scheduler/src/IScheduler.cs
@@ -6,7 +6,7 @@
     /// <summary>Schedules execution of code.</summary>
     public interface IScheduler
     {
-        /// <summary>Schedules a callback to execute at regular intervals.</summary>
+        /// <summary>Schedules a state-accepting callback to execute at regular intervals, starting at a fixed time.</summary>
         /// <param name="callback">The callback to execute.</param>
         /// <param name="state">An object containing information to be used by the callback method.</param>
         /// <param name="startTime">When to first execute the callback. Must be in the future.</param>
@@ -20,7 +20,10 @@
         /// <returns>The <see cref="Timer" /> that was created to execute the callback.</returns>
         Timer ScheduleRecurring(TimerCallback callback, object state, DateTimeOffset startTime, TimeSpan interval);
 
-        /// <summary>Schedules a callback to execute at regular intervals.</summary>
+        /// <summary>
+        ///     Schedules a state-accepting callback to execute at regular intervals, starting after an initial
+        ///     delay.
+        /// </summary>
         /// <param name="callback">The callback to execute.</param>
         /// <param name="state">An object containing information to be used by the callback method.</param>
         /// <param name="startAfter">The amount of time to delay before first executing the callback.</param>
@@ -34,7 +37,7 @@
         /// <returns>The <see cref="Timer" /> that was created to execute the callback.</returns>
         Timer ScheduleRecurring(TimerCallback callback, object state, TimeSpan startAfter, TimeSpan interval);
 
-        /// <summary>Schedules a callback to execute at regular intervals.</summary>
+        /// <summary>Schedules an action to execute at regular intervals, starting at a fixed time.</summary>
         /// <param name="callback">The callback to execute.</param>
         /// <param name="startTime">When to first execute the callback. Must be in the future.</param>
         /// <param name="interval">How often to execute the callback.</param>
@@ -44,7 +47,7 @@
         /// <returns>The <see cref="Timer" /> that was created to execute the callback.</returns>
         Timer ScheduleRecurring(Action callback, DateTimeOffset startTime, TimeSpan interval);
 
-        /// <summary>Schedules a callback to execute at regular intervals.</summary>
+        /// <summary>Schedules an action to execute at regular intervals, starting after an initial delay.</summary>
         /// <param name="callback">The callback to execute.</param>
         /// <param name="startAfter">The amount of time to delay before first executing the callback.</param>
         /// <param name="interval">How often to execute the callback.</param>
@@ -54,7 +57,7 @@
         /// <returns>The <see cref="Timer" /> that was created to execute the callback.</returns>
         Timer ScheduleRecurring(Action callback, TimeSpan startAfter, TimeSpan interval);
 
-        /// <summary>Schedules a callback to execute a single time in the future.</summary>
+        /// <summary>Schedules a state-accepting callback to execute once at a fixed time in the future.</summary>
         /// <param name="callback">The callback to execute.</param>
         /// <param name="state">An object containing information to be used by the callback method.</param>
         /// <param name="startTime">When to execute the callback. Must be in the future.</param>
@@ -66,7 +69,7 @@
         /// <returns>The <see cref="Timer" /> that was created to execute the callback.</returns>
         Timer ScheduleOneTime(TimerCallback callback, object state, DateTimeOffset startTime);
 
-        /// <summary>Schedules a callback to execute a single time in the future.</summary>
+        /// <summary>Schedules a state-accepting callback to execute once after a delay.</summary>
         /// <param name="callback">The callback to execute.</param>
         /// <param name="state">An object containing information to be used by the callback method.</param>
         /// <param name="startAfter">The amount of time to delay before first executing the callback.</param>
@@ -78,7 +81,7 @@
         /// <returns>The <see cref="Timer" /> that was created to execute the callback.</returns>
         Timer ScheduleOneTime(TimerCallback callback, object state, TimeSpan startAfter);
 
-        /// <summary>Schedules a callback to execute a single time in the future.</summary>
+        /// <summary>Schedules an action to execute once at a fixed time in the future.</summary>
         /// <param name="callback">The callback to execute.</param>
         /// <param name="startTime">When to execute the callback. Must be in the future.</param>
         /// <exception cref="ArgumentNullException"><paramref name="callback" /> is <see langword="null" />.</exception>
@@ -86,7 +89,7 @@
         /// <returns>The <see cref="Timer" /> that was created to execute the callback.</returns>
         Timer ScheduleOneTime(Action callback, DateTimeOffset startTime);
 
-        /// <summary>Schedules a callback to execute a single time in the future.</summary>
+        /// <summary>Schedules an action to execute once after a delay.</summary>
         /// <param name="callback">The callback to execute.</param>
         /// <param name="startAfter">The amount of time to delay before first executing the callback.</param>
         /// <exception cref="ArgumentNullException"><paramref name="callback" /> is <see langword="null" />.</exception>
